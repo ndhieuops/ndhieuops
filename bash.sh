b6304622b5b91ea89ff69cd9b050d8d3a475449d
Duy Hieu (Otis) <108449617+ndhieuops@users.noreply.github.com>
2022-10-20 11:27:09 +0700

    Update and rename README.md to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/oracle/docker-images
+
 cd docker-images && git config core.sparseCheckout true && git sparse-checkout init --cone && git sparse-checkout set OracleDatabase/SingleInstance
 cd /OracleDatabase/SingleInstance/dockerfiles/19.3.0
 https://download.oracle.com/otn/linux/oracle19c/190000/LINUX.X64_193000_db_home.zip?AuthParam=1666239845_4db0b302c703b19f30aa02bd26b3d9ee
