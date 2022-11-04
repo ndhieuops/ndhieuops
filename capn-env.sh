f40dae6b10abc4c9c9b1b7c81544e0a3b23985fa
Duy Hieu (Otis) <108449617+ndhieuops@users.noreply.github.com>
2022-11-04 14:59:34 +0700

    Create capn-env.sh

---
+++ capn-env.sh
@@ -0,0 +1,18 @@
+apt update && apt upgrade -y 
+apt install -y git make 
+wget  https://go.dev/dl/go1.19.linux-amd64.tar.gz 
+tar -xvf go1.19.linux-amd64.tar.gz 
+mv go /usr/local 
+export GOROOT=/usr/local/go 
+rm -r go go1.19.linux-amd64.tar.gz
+go version
+snap install kubectl --classic
+snap install kustomize --classic
+snap install google-cloud-cli --classic
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+git clone https://github.com/kubernetes-sigs/cluster-api-provider-nested.git
+git clone https://github.com/kubernetes-sigs/cluster-api.git
+cd cluster-api && git checkout release-0.4 && make clusterctl && chmod +x clusterctl && cp clusterctl /urs/local/bin/clusterctl
+wget https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
