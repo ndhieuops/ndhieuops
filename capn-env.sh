72932bbb1a7d1154aed862ffb55c6c352078b683
Duy Hieu (Otis) <108449617+ndhieuops@users.noreply.github.com>
2022-11-08 10:10:10 +0700

    Update capn-env.sh

--- capn-env.sh
+++ capn-env.sh
@@ -14,8 +14,15 @@ chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 git clone https://github.com/kubernetes-sigs/cluster-api-provider-nested.git
 git clone https://github.com/kubernetes-sigs/cluster-api.git
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=13sxq2IHEWzx8osZHm8C9EcCRxF5dkN7w' -O clusterctl && chmod +x clusterctl && cp clusterctl /urs/local/bin/clusterctl
+cd cluster-api && git checkout release-0.4 && sleep 3 && make clusterctl && cp ./bin/clusterctl /usr/local/bin/clusterctl
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=13sxq2IHEWzx8osZHm8C9EcCRxF5dkN7w' -O clusterctl1.24 && chmod +x clusterctl1.24 && mv clusterctl1.24 /urs/local/bin/clusterctl1.24
 wget https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
 curl -L -o kubebuilder https://go.kubebuilder.io/dl/latest/$(go env GOOS)/$(go env GOARCH)
 chmod +x kubebuilder && mv kubebuilder /usr/local/bin/
+kind create cluster --name=capn
+mkdir user
+mv clusterctl ~
+mv cluster-api-provider-nested ~
+mv cluster-api ~
+mv cert-manager.yaml ~
 
